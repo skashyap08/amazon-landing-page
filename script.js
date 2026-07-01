2e6ae7f3ed6a08cb07d209f1c2669650a3c45eb1
skashyap08 <sakshikashyap9876@gmail.com>
2026-07-01 18:14:22 +0530

    updated script.js-20

--- script.js
+++ script.js
@@ -313,4 +313,27 @@ link.classList.add("active");
 });
 
 });
+const header=document.querySelector("header");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>50){
+
+header.style.background="#091118";
+
+header.style.boxShadow=
+
+"0 10px 25px rgba(0,0,0,.4)";
+
+}
+
+else{
+
+header.style.background="rgba(0,0,0,.5)";
+
+header.style.boxShadow="none";
+
+}
+
+});
 
