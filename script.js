3ba09beb6e7b40b1c21b675f7afb798aad4b84ec
skashyap08 <sakshikashyap9876@gmail.com>
2026-07-01 18:06:11 +0530

    updated script.js-2

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ window.addEventListener("load", () => {
         loader.style.visibility = "hidden";
     }, 1200);
 });
+const hero = document.querySelector(".hero");
+
+const heroImages = [
+    "images/hero1.jpg",
+    "images/hero2.jpg",
+    "images/hero3.jpg"
+];
